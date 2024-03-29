f7aca28ecdcb4e2d27a7967b5b2af577686b0c31
Chris Huang <huan1160@purdue.edu>
2024-03-29 08:52:00 -0400

    Add background blackening effect when toggling menu

--- app/header/Header.js
+++ app/header/Header.js
@@ -2,7 +2,8 @@ import { Stack, useRouter } from "expo-router"
 import { useState } from "react";
 import { COLORS, icons } from "../../constants"
 import { ScreenHeaderBtn } from "../../components";
-import SideMenu from "../../components/common/sidebar/SideMenu"
+import SideMenu from "../../components/common/SideBar/SideMenu"
+import BackDrop from "../../components/common/BackDrop/BackDrop"
 
 const Header = ({ title, rightVisible=true }) => {
     const router = useRouter()
@@ -19,6 +20,7 @@ const Header = ({ title, rightVisible=true }) => {
     
     return (
         <>
+            <BackDrop isVisible={menuVisible} onClick={() => setMenuVisible(false)} />
             <SideMenu isVisible={menuVisible} onClose={() => setMenuVisible(false)} />
             <Stack.Screen
                 options={{
