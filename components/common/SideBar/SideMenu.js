2e39c253317ac019a6cd5681f179dd1d4f3c1fb7
Chris Huang <huan1160@purdue.edu>
2024-03-31 12:07:28 -0400

    Update side bar

--- components/common/SideBar/SideMenu.js
+++ components/common/SideBar/SideMenu.js
@@ -99,7 +99,7 @@ const styles = StyleSheet.create({
   sectionHeader: {
       fontSize: 22,
       fontWeight: 'bold',
-      paddingBottom: 25,
+      paddingBottom: 20,
   },
   headerContent: {
     flexDirection: 'row',
@@ -110,7 +110,7 @@ const styles = StyleSheet.create({
     width: 30,
     height: 30,
     marginRight: 20,
-    marginBottom: 25,
+    marginBottom: 20,
   },
   sectionSubHeader: {
       fontSize: 22,
