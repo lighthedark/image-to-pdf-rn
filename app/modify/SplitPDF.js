14de0ffd3af861c7529f9e28166a7b63a945c85e
rabbit <rabbit@rabbits-Air.attlocal.net>
2024-04-07 22:29:18 -0700

    Created empty page for rest of home screen buttons

---
+++ app/modify/SplitPDF.js
@@ -0,0 +1,27 @@
+import { SafeAreaView, ScrollView, View } from "react-native";
+import { LinearGradient } from 'expo-linear-gradient';
+import { COLORS, DEFAULT_STYLE } from '../../constants';
+import Header from "../header/Header";
+
+const SplitPDF = () => {
+
+  return (
+    <LinearGradient colors={[COLORS.colorPrimary, COLORS.colorAccent]} start={{x: 0, y: 0}} end={{x: 0, y: 0.16}} style={DEFAULT_STYLE}>
+      <SafeAreaView style={DEFAULT_STYLE}>
+        <Header
+          title="Split PDF"
+        />
+        <ScrollView 
+          showsVerticalScrollIndicator={false}
+          style={{borderTopLeftRadius: 25,
+                  borderTopRightRadius: 25,
+                  backgroundColor: COLORS.lighter_gray}}>
+          <View>
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </LinearGradient>
+  );
+};
+
+export default SplitPDF;
